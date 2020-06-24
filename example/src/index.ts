02b3fcd9d97af8ccc78076c95b104bae7f8778fd
Julien Scholz <julien.scholz@sotax.com>
2020-06-24 15:48:47 +0200

    Start proper example code

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,8 +1,13 @@
+import { OneNumber, TwoNumbers } from './generated/service_pb';
 import { ExampleService, IExampleServer } from './generated/service_grpc_pb';
-import { defineService } from 'grpc-rxjs';
+import { defineMethod, defineService } from 'reactive-grpc';
+
+class ExampleServer implements IExampleServer {
+  addTwoNumbers = defineMethod(async function (request: TwoNumbers): Promise<OneNumber> {
+    return new OneNumber().setA(request.getA() + request.getB());
+  });
+}
 
 defineService<IExampleServer>(ExampleService, {
-  addTwoNumbers() {
-
-  }
+  
 });
--- lib/define_method.ts
+++ lib/define_method.ts
@@ -23,14 +23,26 @@ export type ReactiveMethod<RequestType, ResponseType> =
   | ReactiveServerStreamMethod<RequestType, ResponseType>
   | ReactiveBidirectionalStreamMethod<RequestType, ResponseType>;
 
-type DereactifyMethod<RequestType, ResponseType, ReactiveMethodType extends ReactiveMethod<RequestType, ResponseType>> =
-  ReactiveMethodType extends ReactiveUnaryMethod<RequestType, ResponseType>
+type DereactifyMethod<
+  RequestType,
+  ResponseType,
+  ReactiveMethodType extends ReactiveMethod<RequestType, ResponseType>
+> = ReactiveMethodType extends ReactiveUnaryMethod<RequestType, ResponseType>
   ? grpc.handleUnaryCall<RequestType, ResponseType>
-  : ReactiveMethodType extends ReactiveClientStreamMethod<RequestType, ResponseType>
+  : ReactiveMethodType extends ReactiveClientStreamMethod<
+      RequestType,
+      ResponseType
+    >
   ? grpc.handleClientStreamingCall<RequestType, ResponseType>
-  : ReactiveMethodType extends ReactiveServerStreamMethod<RequestType, ResponseType>
+  : ReactiveMethodType extends ReactiveServerStreamMethod<
+      RequestType,
+      ResponseType
+    >
   ? grpc.handleServerStreamingCall<RequestType, ResponseType>
-  :ReactiveMethodType extends ReactiveBidirectionalStreamMethod<RequestType, ResponseType>
+  : ReactiveMethodType extends ReactiveBidirectionalStreamMethod<
+      RequestType,
+      ResponseType
+    >
   ? grpc.handleBidiStreamingCall<RequestType, ResponseType>
   : never;
 
@@ -38,6 +50,12 @@ export function defineMethod<
   RequestType,
   ResponseType,
   ReactiveMethodType extends ReactiveMethod<RequestType, ResponseType>
->(method: ReactiveMethodType): DereactifyMethod<RequestType, ResponseType, ReactiveMethodType> {
-  return null as unknown as DereactifyMethod<RequestType, ResponseType, ReactiveMethodType>;
+>(
+  method: ReactiveMethod<RequestType, ResponseType>
+): DereactifyMethod<RequestType, ResponseType, ReactiveMethodType> {
+  return (null as unknown) as DereactifyMethod<
+    RequestType,
+    ResponseType,
+    ReactiveMethodType
+  >;
 }
