7906122b25f57b98c818d5526ae3f41437b477d0
Julien Scholz <julienscholzo@hotmail.de>
2020-06-25 12:17:15 +0200

    Add example services and fix bugs

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,16 +3,48 @@ import {map, reduce} from 'rxjs/operators';
 import { OneNumber, TwoNumbers, Empty } from './generated/service_pb';
 import { ExampleService, IExampleServer } from './generated/service_grpc_pb';
 import { defineMethod, defineService } from 'reactive-grpc';
+import * as grpc from 'grpc';
 
 class ExampleServer implements IExampleServer {
   addTwoNumbers = defineMethod(async function (request: TwoNumbers): Promise<OneNumber> {
     return new OneNumber().setA(request.getA() + request.getB());
+  });
+  addStreamOfNumbers = defineMethod(function (request: Observable<OneNumber>): Promise<OneNumber> {
+    return request.pipe(
+      reduce((acc, value) => acc + value.getA(), 0),
+      map((value) => new OneNumber().setA(value)),
+    ).toPromise();
+  });
+  getFibonacciSequence = defineMethod(function (request: Empty): Observable<OneNumber> {
+    let a = 0;
+    let b = 1;
+    return interval(1000).pipe(map(() => {
+      let next = a + b;
+      a = b;
+      b = next;
+      return new OneNumber().setA(next)
+    }));
+  });
+  runningAverage = defineMethod(function (request: Observable<OneNumber>): Observable<OneNumber> {
+    let average = 0;
+    return request.pipe(
+      map((value, index) => {
+        average = (value.getA() + index * average) / (index + 1);
+        return new OneNumber().setA(average);
+      })
+    );
   });
 }
 
 defineService<IExampleServer>(ExampleService, {
   async addTwoNumbers(request: TwoNumbers): Promise<OneNumber> {
     return new OneNumber().setA(request.getA() + request.getB());
+  },
+  addStreamOfNumbers(request: Observable<OneNumber>): Promise<OneNumber> {
+    return request.pipe(
+      reduce((acc, value) => acc + value.getA(), 0),
+      map((value) => new OneNumber().setA(value)),
+    ).toPromise();
   },
   getFibonacciSequence(request: Empty): Observable<OneNumber> {
     let a = 0;
@@ -21,14 +53,8 @@ defineService<IExampleServer>(ExampleService, {
       let next = a + b;
       a = b;
       b = next;
-      return new OneNumber().setA(next);
+      return new OneNumber().setA(next)
     }));
-  },
-  addStreamOfNumbers(request: Observable<OneNumber>): Promise<OneNumber> {
-    return request.pipe(
-      reduce((acc, value) => acc + value.getA(), 0),
-      map((value) => new OneNumber().setA(value)),
-    ).toPromise();
   },
   runningAverage(request: Observable<OneNumber>): Observable<OneNumber> {
     let average = 0;
--- lib/define_method.ts
+++ lib/define_method.ts
@@ -15,7 +15,7 @@ export type ReactiveServerStreamMethod<RequestType, ResponseType> = (
 
 export type ReactiveBidirectionalStreamMethod<RequestType, ResponseType> = (
   request: Observable<RequestType>
-) => Observable<Response>;
+) => Observable<ResponseType>;
 
 export type ReactiveMethod<RequestType, ResponseType> =
   | ReactiveUnaryMethod<RequestType, ResponseType>
@@ -24,38 +24,31 @@ export type ReactiveMethod<RequestType, ResponseType> =
   | ReactiveBidirectionalStreamMethod<RequestType, ResponseType>;
 
 type DereactifyMethod<
-  RequestType,
-  ResponseType,
-  ReactiveMethodType extends ReactiveMethod<RequestType, ResponseType>
-> = ReactiveMethodType extends ReactiveUnaryMethod<RequestType, ResponseType>
-  ? grpc.handleUnaryCall<RequestType, ResponseType>
-  : ReactiveMethodType extends ReactiveClientStreamMethod<
-      RequestType,
-      ResponseType
-    >
-  ? grpc.handleClientStreamingCall<RequestType, ResponseType>
+  ReactiveMethodType extends ReactiveMethod<any, any>
+> = ReactiveMethodType extends ReactiveBidirectionalStreamMethod<
+  infer RequestType,
+  infer ResponseType
+>
+  ? grpc.handleBidiStreamingCall<RequestType, ResponseType>
   : ReactiveMethodType extends ReactiveServerStreamMethod<
-      RequestType,
-      ResponseType
+      infer RequestType,
+      infer ResponseType
     >
   ? grpc.handleServerStreamingCall<RequestType, ResponseType>
-  : ReactiveMethodType extends ReactiveBidirectionalStreamMethod<
-      RequestType,
-      ResponseType
+  : ReactiveMethodType extends ReactiveClientStreamMethod<
+      infer RequestType,
+      infer ResponseType
     >
-  ? grpc.handleBidiStreamingCall<RequestType, ResponseType>
+  ? grpc.handleClientStreamingCall<RequestType, ResponseType>
+  : ReactiveMethodType extends ReactiveUnaryMethod<
+      infer RequestType,
+      infer ResponseType
+    >
+  ? grpc.handleUnaryCall<RequestType, ResponseType>
   : never;
 
 export function defineMethod<
-  RequestType,
-  ResponseType,
-  ReactiveMethodType extends ReactiveMethod<RequestType, ResponseType>
->(
-  method: ReactiveMethod<RequestType, ResponseType>
-): DereactifyMethod<RequestType, ResponseType, ReactiveMethodType> {
-  return (null as unknown) as DereactifyMethod<
-    RequestType,
-    ResponseType,
-    ReactiveMethodType
-  >;
+  ReactiveMethodType extends ReactiveMethod<any, any>
+>(method: ReactiveMethodType): DereactifyMethod<ReactiveMethodType> {
+  return (null as unknown) as DereactifyMethod<ReactiveMethodType>;
 }
