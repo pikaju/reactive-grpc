3c2ab53f5835bc2e2b6244071110e6bd72062bcb
Julien Scholz <julienscholzo@hotmail.de>
2020-06-25 10:32:14 +0200

    Add example service

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,4 +1,6 @@
-import { OneNumber, TwoNumbers } from './generated/service_pb';
+import {interval, Observable} from 'rxjs';
+import {map, reduce} from 'rxjs/operators';
+import { OneNumber, TwoNumbers, Empty } from './generated/service_pb';
 import { ExampleService, IExampleServer } from './generated/service_grpc_pb';
 import { defineMethod, defineService } from 'reactive-grpc';
 
@@ -9,5 +11,32 @@ class ExampleServer implements IExampleServer {
 }
 
 defineService<IExampleServer>(ExampleService, {
-  
+  async addTwoNumbers(request: TwoNumbers): Promise<OneNumber> {
+    return new OneNumber().setA(request.getA() + request.getB());
+  },
+  getFibonacciSequence(request: Empty): Observable<OneNumber> {
+    let a = 0;
+    let b = 1;
+    return interval(1000).pipe(map(() => {
+      let next = a + b;
+      a = b;
+      b = next;
+      return new OneNumber().setA(next);
+    }));
+  },
+  addStreamOfNumbers(request: Observable<OneNumber>): Promise<OneNumber> {
+    return request.pipe(
+      reduce((acc, value) => acc + value.getA(), 0),
+      map((value) => new OneNumber().setA(value)),
+    ).toPromise();
+  },
+  runningAverage(request: Observable<OneNumber>): Observable<OneNumber> {
+    let average = 0;
+    return request.pipe(
+      map((value, index) => {
+        average = (value.getA() + index * average) / (index + 1);
+        return new OneNumber().setA(average);
+      })
+    );
+  },
 });
