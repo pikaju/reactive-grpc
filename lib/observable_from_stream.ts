c72f39fe896584d55c96826616a014c0737e852c
Julien Scholz <julienscholzo@hotmail.de>
2020-06-27 16:11:38 +0200

    Fix observableFromStream bug

--- lib/observable_from_stream.ts
+++ lib/observable_from_stream.ts
@@ -2,17 +2,26 @@ import { Observable } from "rxjs";
 
 export function observableFromStream<T>(stream: NodeJS.ReadableStream) {
   return new Observable<T>((subscriber) => {
+    function dataHandler(data: any) {
+      subscriber.next(data);
+    }
+
     function errorHandler(error: any) {
       subscriber.error(error);
       subscriber.complete();
     }
-    stream.on("data", subscriber.next);
+
+    function endHandler() {
+      subscriber.complete();
+    }
+
+    stream.on("data", dataHandler);
     stream.on("error", errorHandler);
-    stream.on("end", subscriber.complete);
+    stream.on("end", endHandler);
     return () => {
-      stream.removeListener("data", subscriber.next);
+      stream.removeListener("data", dataHandler);
       stream.removeListener("error", errorHandler);
-      stream.removeListener("end", subscriber.complete);
+      stream.removeListener("end", endHandler);
     };
   });
 }
