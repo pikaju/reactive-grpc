57bfe32483bc5c2eadeebc4773906f6de8531036
Julien Scholz <julien.scholz@sotax.com>
2021-02-25 14:27:52 +0100

    QV5-404 Properly export new Metadata class

--- lib/node/index.ts
+++ lib/node/index.ts
@@ -3,5 +3,6 @@ export * from '../common/error';
 export * from './client_methods';
 export * from './define_method';
 export * from './define_service';
+export * from './metadata';
 export * from './reactify_client';
 export * from './server_methods';
